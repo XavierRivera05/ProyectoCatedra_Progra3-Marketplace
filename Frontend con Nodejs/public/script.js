108a77649bec031a0779c65f0f67b77d453f219c
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-04-15 16:32:25 -0600

    Parche de diseño en Bootstrap
    eldiablomano

--- Frontend con Nodejs/public/script.js
+++ Frontend con Nodejs/public/script.js
@@ -4,13 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
     .then(data => {
       console.log("Productos recibidos:", data);
 
-      const contenedor = document.getElementById('contenedor-productos'); // Asegúrate que este div exista
+      const contenedor = document.getElementById('contenedor-productos'); //efectivamente el div existe
 
       data.forEach(producto => {
         const card = document.createElement('div');
-        card.classList.add('producto-card'); // por si luego se usa el coso de carritos
+        card.classList.add('producto-card','col-md-4', 'col-lg-3', 'd-flex'); // para que convivan Bootstrap y CSS XD
        
-        //esto es del carrito XD
+        //diseño de productos en Bootstrap carechimba
         card.innerHTML = `
         <div class="card shadow-sm" style="width: 18rem;">
           <img src="${producto.imagen_url}" class="card-img-top" alt="${producto.nombre}">
@@ -36,6 +36,6 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     })
     .catch(error => {
-      console.error("Error al cargar los productos :(", error);
+      console.error("error al cargar los productos mi cuete :(", error);
     });
 });
